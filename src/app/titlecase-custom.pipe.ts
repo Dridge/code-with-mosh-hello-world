6b0af9b43e2e45630be3809b2ef85df6fdc4ce89
Dridge <richard.eldridge2010@gmail.com>
2021-07-02 19:14:31 +0100

    Simplied and improved transform method

--- src/app/titlecase-custom.pipe.ts
+++ src/app/titlecase-custom.pipe.ts
@@ -21,22 +21,49 @@ export class TitlecaseCustomPipe implements PipeTransform {
   //             txt[0].toUpperCase() + txt.substr(1).toLowerCase()))
   // }
 
-  //Moshis solution
-  transform(value: String): any {
-    if (!value) return null;
+  //Moshis first solution
+  // transform(value: String): any {
+  //   if (!value) return null;
 
+  //   let prepositions = [
+  //     'of','the','an','a','yet','nor','but','so','by','on','in'
+  //   ];
+  //   let words = value.split(' ');
+  //   for (var i = 0; i < words.length; i++) {
+  //     if(i > 0 && prepositions.includes(words[i].toLowerCase()))  {
+  //       words[i] = words[i].toLowerCase();
+  //     } else {
+  //       words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
+  //     }
+  //   }
+
+  //   return words.join(' ');
+  // }
+  //Moshis second solution
+  transform(value: String): any {
+    if (!value) 
+      return null;
+
+    let words = value.split(' ');
+    for (var i = 0; i < words.length; i++) {
+      let word = words[i];
+      if(i > 0 && this.isPreposition(word))  {
+        word = word.toLowerCase();
+      } else {
+        word = this.toTitleCase(word);
+      }
+    }
+    return words.join(' ');
+  }
+
+  private isPreposition(word: string): boolean {
     let prepositions = [
       'of','the','an','a','yet','nor','but','so','by','on','in'
     ];
-    let words = value.split(' ');
-    for (var i = 0; i < words.length; i++) {
-      if(i > 0 && prepositions.includes(words[i]))  {
-        words[i] = words[i].toLowerCase();
-      } else {
-        words[i] = words[i].substr(0, 1).toUpperCase() + words[i].substr(1).toLowerCase();
-      }
-    }
+    return prepositions.includes(word.toLowerCase());
+  }
 
-    return words.join(' ');
+  private toTitleCase(word: String): string {
+    return word.substr(0, 1).toUpperCase() + word.substr(1).toLowerCase();
   }
 }
