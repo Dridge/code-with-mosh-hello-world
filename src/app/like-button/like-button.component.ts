32b199f499afe0529453ae1ee86f354a9d9780c4
Dridge <richard.eldridge2010@gmail.com>
2021-07-29 08:22:31 +0100

    Working like button

--- src/app/like-button/like-button.component.ts
+++ src/app/like-button/like-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'like-button',
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./like-button.component.css']
 })
 export class LikeButtonComponent implements OnInit {
+  @Input('isSelected') 
   isSelected: boolean = false;
+
+  @Input('isHovered') 
+  isHovered: boolean = false;
+
+  @Output()
+  mouseOverEvent = new EventEmitter();
 
   constructor() { 
 
@@ -19,10 +26,14 @@ export class LikeButtonComponent implements OnInit {
 
   onHover() {
     console.log("Favourite icon hovered on");
+    this.isHovered = !this.isHovered
+    this.mouseOverEvent.emit({newValue: this.isHovered});
   }
 
   onHoverOff() {
     console.log("Favourite icon hovered off");
+    this.isHovered = !this.isHovered
+    this.mouseOverEvent.emit({newValue: this.isHovered});
   }
 
   ngOnInit(): void {
